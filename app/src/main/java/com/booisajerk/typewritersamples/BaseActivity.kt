b818242795ff21940a4da7e4b677d9dd4ff3ea98
Jenn Parker <jenniferparker070@gmail.com>
2018-09-18 13:28:19 -0700

    Remove overflow menu

--- app/src/main/java/com/booisajerk/typewritersamples/BaseActivity.kt
+++ app/src/main/java/com/booisajerk/typewritersamples/BaseActivity.kt
@@ -13,23 +13,4 @@ open class BaseActivity : AppCompatActivity() {
     private val TAG = "Parker" + BaseActivity::class.qualifiedName
 
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        Log.d(TAG, "onCreateOptionsMenu called")
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-
-        if (id == R.id.action_settings) return true
-
-        if (id == R.id.action_search) {
-            val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
-        }
-        return if (id == R.id.action_credits) true
-        else super.onOptionsItemSelected(item)
-
-    }
 }
